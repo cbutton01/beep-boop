35049944c64117572fcf7c354ffe7e3ab14442c6
cb <csbutton01@hotmail.com>
2018-03-30 13:19:33 -0700

    finalOutput function meets all objectives

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,9 @@
-function beep (input){
-  if (input === 0) {
-    return 'beep';
-  } else {
-    return input;
-  }
-}
-
-function boop (input) {
-  if (input === 1) {
-    return 'boop';
-  } else {
-    return input;
-  }
-}
-
 function hal (){
   return "I'm sorry, Dave.  I'm afraid I can't do that.";
 }
 
 function makeArray (input) {
+  // debugger;
   var currentNumber = 0;
   var numberArray = [];
   for (var i = 0; i < input; i++) { //recieve input and push all numbers until input as a string to numberArray
@@ -26,16 +11,22 @@ function makeArray (input) {
     currentNumber++; //increase currentNumber by one
   }
   numberArray.push(''+input); //push input as string to numberArray
-  for (var index = 0; index <= numberArray.length; index++){
-    var arrayForOtherNumbers = numberArray[index].split(''); //split current string into new array to look for ones and zero's
-    for (var j = 0; j < arrayForOtherNumbers.length; j++) {
-      if (arrayForOtherNumbers[j].indexOf('1') > -1) {
-        numberArray[index].splice(index, 1, 'boop');
-      } else if (arrayForOtherNumbers[j].indexOf('0') > -1) {
-        numberArray[index].splice(index, 1, 'beep');
-      }
+
+
+
+  numberArray.forEach(function(string){
+    var parsed = parseInt(string);
+    var splitString = string.split('');
+    if (parsed % 3 === 0 && parsed != 0) {
+      numberArray.splice(string, 1, "I'm sorry, Dave.  I'm afraid I can't do that.");
+    } else if (splitString.indexOf('1') > -1) {
+      numberArray.splice(string, 1, 'boop');
+    } else if (splitString.indexOf('0') > -1) {
+      numberArray.splice(string, 1, 'beep');
     }
-  } // parent for loop
+  });
+
+  return numberArray;
 } // end of makeArray function
 
 function finalOutput (input) {
