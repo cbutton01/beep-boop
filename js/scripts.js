36b82a4d8e05ec84aa0eb61784722a2690d05951
cb <csbutton01@hotmail.com>
2018-03-30 08:49:43 -0700

    connect scripts.js to index.html and add beep, boop, and hal functions

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function beep (input){
+  if (input === 0) {
+    return 'beep';
+  }
+}
+
+function boop (input) {
+  if (input === 1) {
+    return 'boop';
+  }
+}
+
+function hal (input){
+  if (input === 3) {
+    return "I'm sorry, Dave.  I'm afraid I can't do that.";
+  }
+}
