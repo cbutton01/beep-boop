3f2cd6975ad215865228f483240da26839399ca6
cb <csbutton01@hotmail.com>
2018-03-30 10:09:53 -0700

    add comments to new for loops to understand what each loop is doing

--- js/scripts.js
+++ js/scripts.js
@@ -20,16 +20,25 @@ function hal (){
 
 function makeArray (input) {
   var currentNumber = 0;
-  var numberArray = (""+input).split(' ');
-  for (var i = 0; i < input; i++) {
-
+  var arrayForOtherNumbers = []; //array to check for numbers containing zero's and one's
+  for (var i = 0; i < input; i++) { //recieve input and push all numbers until input as a string to numberArray
+    numberArray.push(''+currentNumber);
+    currentNumber++; //increase currentNumber by one
   }
-}
+  numberArray.push(''+input); //push input as string to numberArray
+  for (var index = 0; index <= numberArray.length; index++){
+    var numberArray = numberArray[index].split('') //split current string into new array to look for ones and zero's
+
+    for (var j = 0; j <= arrayForOtherNumbers.length; j++) {
+      arrayForOtherNumbers[j]
+    } //nested for loop
+  } // parent for loop
+} // end of makeArray function
 
 function finalOutput (input) {
-  if (input % 3 === 0){
+  if (input % 3 === 0){ //if the input is divisible by 3 then HAL will respond
     return  hal();
-  } else {
+  } else { //else feed the input to the makeArray function
     return makeArray(input);
   }
 }
