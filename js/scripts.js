703c2730db3a35d00af82eb171174f0c261e39a4
cb <csbutton01@hotmail.com>
2018-03-30 08:57:45 -0700

    makeArray function returns the input as an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 function beep (input){
   if (input === 0) {
     return 'beep';
+  } else {
+    return input;
   }
 }
 
 function boop (input) {
   if (input === 1) {
     return 'boop';
+  } else {
+    return input;
   }
 }
 
 function hal (input){
   if (input === 3) {
     return "I'm sorry, Dave.  I'm afraid I can't do that.";
+  } else {
+    return input;
   }
+}
+
+function makeArray (input) {
+  var numberArray = (""+input).split('');
+  return numberArray;
 }
